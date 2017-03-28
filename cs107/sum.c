09e1d050a59ba6c8ec137b8e20fdd7b26a9333a1
jxunhappy <lookto2008@gmail.com>
2017-03-28 20:01:15 +0800

    add write and write main function in sum.c

--- cs107/sum.c
+++ cs107/sum.c
@@ -5,3 +5,10 @@ int sum_array(int arr[], int nelems) {
     }
     return sum;
 }
+
+int main()
+{
+    int arr[5] = {1,2,3,4,5};
+    sum_array(arr, 5);
+    return 0;
+}
---
+++ cs107/write.c
@@ -0,0 +1,6 @@
+int main()
+{
+    write(1, "Hello, world\n", 13);
+    _exit(0);
+    return 0;
+}
