815999e2697f7b68bb9d7e981edaf0970262e004
jxunhappy <lookto2008@gmail.com>
2017-04-09 20:59:54 +0800

    merge sort

---
+++ algo/mergesort.cpp
@@ -0,0 +1,88 @@
+#include <vector>
+#include <iostream>
+
+// merge arr[p, q) and arr[q, r) into arr[p, r)
+// total(r - p) elements.
+void Merge(std::vector<int>& arr, size_t p, size_t q, size_t r)
+{
+    size_t i = p;        // array start index
+    size_t n1 = q - p;   // left size
+    size_t n2 = r - q;   // right size
+
+    std::vector<int> left;
+    std::vector<int> right;
+    while(p < q) {
+        left.push_back(arr[p]);
+        ++p;
+    }
+    while(q < r) {
+        right.push_back(arr[q]);
+        ++q;
+    }
+
+    p = 0;
+    q = 0;
+    while(i < r) {
+        if(p >= n1 || q >= n2)
+            break;
+        if(left[p] < right[q]) {
+            arr[i] = left[p];
+            ++p;
+        } else {
+            arr[i] = right[q];
+            ++q;
+        }
+        ++i;
+    }
+
+    while(p < n1) {
+        arr[i] = left[p];
+        ++i;
+        ++p;
+    }
+
+    while(q < n2) {
+        arr[i] = right[q];
+        ++i;
+        ++q;
+    };
+}
+
+/*
+ * must: p less or equal r.
+ * range in arr as [p,r)
+ * include p but not r.
+ */
+void MergeSortHelper(std::vector<int>& arr, size_t p, size_t r)
+{
+    // p == r: no elemnts
+    // r - p == 1: 1 elements
+    // in the two case above arr is sorted.
+    if(r - p > 1)
+    {
+        // because: p <= r -> p + r < r + r < 2r
+        // so: q = (p + r)/2 < 2r/2 = r
+        size_t q = (p + r)/2;
+        // sub problem one, range [p, q)
+        MergeSortHelper(arr, p, q);
+        // sub problem two, range [q, r)
+        MergeSortHelper(arr, q, r);
+        // merge p <= q < r
+        Merge(arr, p, q, r);
+    }
+}
+
+void MergeSort(std::vector<int>& arr)
+{
+    MergeSortHelper(arr, 0, arr.size());
+}
+
+int main()
+{
+    std::vector<int> arr{2, 4, 5, 7, 1, 2, 3, 6};
+    MergeSort(arr);
+    for(auto& e : arr)
+        std::cout << e;
+    std::cout << std::endl;
+    return 0;
+}
