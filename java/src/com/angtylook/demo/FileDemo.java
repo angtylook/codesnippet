677ac8fb31e5db8b3e0019831e1473d1a7d14a27
jxunhappy <lookto2008@gmail.com>
2017-04-22 08:18:47 +0800

    add java directory

---
+++ java/src/com/angtylook/demo/FileDemo.java
@@ -0,0 +1,10 @@
+// javac -s src -d bin src/com/angtylook/demo/FileDemo.javac
+// java -d bin com/angtylook/demo/FileDemo
+
+package com.angtylook.demo;
+
+public class FileDemo {
+  public static void main(String[] args) {
+    System.out.println("hello world");
+  }
+}
