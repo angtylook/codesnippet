13c51351d49ac17d90931e8f9e31f667e3673491
zhangzhiwei <zhang@freebsd.antylook.com>
2017-06-02 00:42:22 +0800

    rvref

--- cpp/rvref.cpp
+++ cpp/rvref.cpp
@@ -10,8 +10,25 @@ void call(std::function<void()>&& func)
     func();
 }
 
+class Widget {
+    public:
+        std::string val_;
+};
+
+void rw(Widget&& w)
+{
+    WhichType<decltype(w)> wtype;
+    WhichType<decltype(w.val_)> mtype;
+    WhichType<decltype(std::move(w.val_))> vtype;
+    WhichType<decltype(std::forward<std::string>(w.val_))> ftype;
+    std::string lval;
+    WhichType<decltype(lval)> ltype;
+}
+
 int main()
 {
+    Widget w;
+    rw(std::move(w));
     std::string obj("hello world");
     auto func = [&obj]() {
         std::cout << obj << std::endl;
