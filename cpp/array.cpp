cc7368c594d95a2873792532b46a43b00120e17e
jxhappy <lookto2008@gmail.com>
2019-05-29 22:39:41 +0800

    array initialize and copy

--- cpp/array.cpp
+++ cpp/array.cpp
@@ -2,16 +2,13 @@
 #include <array>
 #include <iostream>
 #include <stdio.h>
-#include <boost/noncopyable.hpp>
-#include "boost/stacktrace.hpp"
-#include "boost/beast.hpp"
 
-class Demo : public boost::noncopyable
+class Demo
 {
 public:
     Demo()
     {
-        arr_.fill(nullptr);
+        // arr_.fill(nullptr);
     }
     void showArray()
     {
@@ -22,13 +19,39 @@ public:
     }
 private:
     static constexpr int kArraySize = 10;
-    std::array<char*, kArraySize> arr_;
+    std::array<char*, kArraySize> arr_{};  // fill with nullptr
 };
+
+class CArray {
+public:
+    CArray();
+    char name[10];  // will copy
+};
+
+CArray::CArray()
+{
+    name[0] = 'h';
+    name[1] = 'e';
+    name[2] = 'l';
+    name[3] = 'l';
+    name[4] = 'o';
+    name[5] = '\0';
+}
 
 int main()
 {
     Demo demo;
     demo.showArray();
-    boost::beast::http::request req;
+
+    CArray arr1;
+    CArray arr2(arr1);
+    arr2.name[0] = 'f';
+    CArray arr3;
+    arr3 = arr2;
+    std::cout << "arr1:" << arr1.name << std::endl;
+    std::cout << "arr2:" << arr2.name << std::endl;
+    std::cout << "arr3:" << arr3.name << std::endl;
     return 0;
 }
+
+
