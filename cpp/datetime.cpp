982c5849324954e1a94ff4305d5ec387b102f783
zhangzhiwei <zhangzhiwei@huya.com>
2021-10-16 19:18:57 +0800

    strftime

--- cpp/datetime.cpp
+++ cpp/datetime.cpp
@@ -4,6 +4,7 @@
 
 #include <chrono>
 #include <iostream>
+#include <ctime>
 
 int main()
 {
@@ -14,5 +15,11 @@ int main()
               << '/' << std::chrono::system_clock::period::den
               << std::endl;
 
+    std::time_t startTime = 1634036907;
+    char buffer[100] = {};
+    auto n = std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", std::localtime(&startTime));
+    buffer[n] = '\0';
+    std::string out = buffer;
+    std::cout << out << std::endl;
     return 0;
 }
